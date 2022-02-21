295828f54121681e407785ae1e840fe10fad4d9f
Sampada <sampadahakke102@gmail.com>
2022-02-21 21:31:27 +0530

    [Sampada] Adding into sorted list

--- MyDataStructure/Program.cs
+++ MyDataStructure/Program.cs
@@ -12,51 +12,13 @@ namespace MyDataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("=============WELCOME TO DATA STRUCTURE===============");
-            UnorderedList<int> list = new UnorderedList<int>();
-            Console.WriteLine("\nCreating A New List - 56,30,70");
-            list.Append(56);
-            list.Append(30);
-            list.Append(70);
-            Console.WriteLine("Inserting 40 after 30");
-            list.Insert(list.Index(30) + 1, 40);
+            OrderedList<int> list = new OrderedList<int>();
+            Console.WriteLine("Adding 56,30,70 in list");
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
             list.Display();
-            Console.WriteLine("Removing 40 after 30");
-            Console.WriteLine("\nSearch result for 40:" + " " + list.Search(40));
-            list.Remove(40);
-            Console.WriteLine("Size:" + " " + list.Size());
-            Console.ReadKey()
-
-
-            /*Console.WriteLine("Inserting 40 after 30");
-            list.Insert(list.Index(30) + 1, 40);
-            list.Display();
-            *//*  Console.WriteLine("\nSearch result for 30:"+" " + list.Search(30));
-              Console.WriteLine("\nSearch result for 90:"+" " + list.Search(90));*//*
-            Console.ReadKey()*/;
-
-            
-
-
-            //Console.WriteLine("\nDeleting last element");
-            //list.PopLast();
-            //list.Display();
-            //Console.ReadKey();
-
-            //Console.WriteLine("Adding 56,30,70 to list");
-            //list.Add(76);
-            //list.Add(30);
-            //list.Add(56);
-            //list.Display();
-            //Console.ReadKey();
-
-            //Console.WriteLine("Inserting 30 between 56 and 70");
-            //list.Append(56);
-            //list.Append(70);
-            //list.Insert(1, 30);
-            //list.Display();
-            //Console.ReadKey();
-
-
+            Console.ReadKey();
         }
     }
 }
--- MylistLibrary/UnorderedList.cs
+++ MylistLibrary/UnorderedList.cs
@@ -6,171 +6,50 @@ using System.Threading.Tasks;
 
 namespace MylistLibrary
 {
-    public class UnorderedList<T> where T : IComparable<T> 
+    public class UnorderedList<T> : MyLinkedList<T> where T : IComparable
     {
         //Variables
         public Node<T> Head;
 
-        //Initializing Variable With the help of Constructor
-        public UnorderedList()
-        {
-            Head = null;
-        }
-
-        //Creates and Returns empty list
-        public UnorderedList<T> List()
+       
+       //Creates and Returns empty list
+        public static UnorderedList<T> List()
         {
             UnorderedList<T> emptyList = new UnorderedList<T>();
             return emptyList;
         }
 
         //Method used to create list
-        public void Append(T Data)
+        public new void Append(T Data)
         {
-            Node<T> node = new Node<T>(Data);
-            if (Head == null)
-                Head = node;
-            else
-            {
-                Node<T> temp = Head;
-                while (temp.Next != null)
-                    temp = temp.Next;
-                temp.Next = node;
-            }
-        }
-
-        //Method used to display the list
-        public void Display()
-        {
-            Node<T> temp = Head;
-            while (temp != null)
-            {
-                Console.Write(temp.Data+" ");
-                temp = temp.Next;
-            }
+            base.Append(Data);
         }
 
         //Method used to add data in list
-        public void Add(T Data)
+        public new void Add(T Data)
         {
-            Node<T> node = new Node<T>(Data);
-            if (Head == null)
-                Head = node;
-            else
-            {
-                node.Next = Head;
-                Head = node;
-            }
+            base.Add(Data); 
         }
 
         //Method used to insert 30 between 56 and 76
-        public void Insert(int pos, T data)
+        public new void Insert(int pos, T data)
         {
-            Node<T> temp = Head;
-            if (pos < 0)
-            {
-                Console.WriteLine("Invalid position");
-                return;
-            }
-            if (pos == 0)
-                Add(data);
-            else
-            {
-                Node<T> node = new Node<T>(data);
-                for (int i = 1; i < pos; i++)
-                    temp = temp.Next;
-                node.Next = temp.Next;
-                temp.Next = node;
-            }
+           base.Insert(pos, data);  
         }
 
         //Method used to delete first element
-        public void Pop()
+        public new void Pop()
         {
-            if (Head == null)
-                return;
-            else
-                Head = Head.Next;
+            base.Pop();
         }
 
         //Method used to delete last elememt
-        public void PopLast()
+        public new void PopLast()
         {
-            if (Head == null)
-                return;
-            if (Head.Next == null)
-                Head = null;
-            else
-            {
-                Node<T> temp = Head;
-                while (temp.Next.Next != null)
-                    temp = temp.Next;
-                temp.Next = null;
-            }
+            base.PopLast();
 
         }
 
-        //Method used to search element
-        public bool Search(T Data)
-        {
-            Node<T> temp = Head;
-            while (temp != null)
-            {
-                if (temp.Data.CompareTo(Data) == 0)
-                    return true;
-                temp = temp.Next;
-            }
-            return false;
-        }
-
-        public int Index(T Data)
-        {
-            int index = 0;
-            Node<T> temp = Head;
-            while (temp != null)
-            {
-                if (temp.Data.CompareTo(Data) == 0)
-                    return index;
-                temp = temp.Next;
-                index++;
-            }
-            return -1;
-        }
-
-        //Method used to remove element of list
-        public void Remove(T Data)
-        {
-            if (Head == null)
-                return;
-            if (Head.Data.CompareTo(Data) == 0)
-                Head = Head.Next;
-            else
-            {
-                Node<T> temp = Head;
-                while (temp != null)
-                {
-                    if (temp.Next.Data.CompareTo(Data) == 0)
-                    {
-                        temp.Next = temp.Next.Next;
-                        return;
-                    }
-                    temp = temp.Next;
-                }
-            }
-        }
-
-        //Method used to  getting size of list
-        public int Size()
-        {
-            int Size = 0;
-            Node<T> temp = Head;
-            while(temp != null)
-            {
-                temp=temp.Next;
-                Size++;
-            }
-            return Size;
-        }
     }
 }
 
